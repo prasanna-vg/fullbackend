0c079e8b20b5d9a2543b26febc30c35eada1e37e
Prasanna Venugopal <56516795+prasanna-vg@users.noreply.github.com>
2023-08-19 16:42:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -46,17 +46,17 @@ const route = require('./routes/route')
 app.use('/api',route)
 //testing Server
 app.get('/',(req,res)=>{
-    res.send('vanako');
+    res.send('Data Table');
 });
 app.listen(port,()=>{
     console.log("Server started at localhost://3000");
 });
 // Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
+//if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static('client/build'));
+//    app.use(express.static('client/build'));
   
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
+//    app.get('*', (req, res) => {
+//      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+ //   });
+//  }
